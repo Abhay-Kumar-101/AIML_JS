581b68a7144578c27ef1276e50e5733600d8b0eb
ABHAY-KUMAR-101 <abhaykumar101007@gmail.com>
2026-02-20 14:12:46 +0530

    java introduction

--- script.js
+++ script.js
@@ -1,2 +1,39 @@
 console.log("Hello World");
 document.write("Hello World");
+<script type="text/javascript">
+var num=10;
+console.log(num);
+console.log(typeof(num))
+num=false;
+console.log(num);
+console.log(type(num));
+num="abhay kumar";
+consolelog(num);
+console.log(type(num));
+num=null;
+console.log(num);
+cinsole.log(type(num));
+num=undefine;
+console.log(typeof(num));
+
+
+{/* //function    */}
+var sayHello=function(){
+    alert("hello world")
+}
+{/* //array */}
+var numberArray=[1,2,3];
+{/* 
+//object  */}
+
+const sym1=symbol(4)
+const.log(sym1);
+
+const sym2=symbol(4);
+console.log(sym2);
+if (sym1==sym2){
+    console.log("true")
+}
+else
+console.log("false")
+</script>
