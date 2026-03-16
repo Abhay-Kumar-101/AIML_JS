37892454a3b26ae1ccdf7e5850feaf614e9fa7be
ABHAY-KUMAR-101 <abhaykumar101007@gmail.com>
2026-03-16 15:45:32 +0530

    getElements and Events

---
+++ getElement.js
@@ -0,0 +1,10 @@
+// const result1=document.getElementById("demo");
+// const result2=document.getElementsByClassName("demo");
+// const result3=document.getElementsByTagName("p");
+// const result4=document.querySelector("#demo");
+// const result5=document.querySelectorAll(".demo");
+//     for (x in Text(2)){
+//         text[2].style.backgroundColor="yellow";
+//         text[2].style.fontSize="200%";
+//         text[2].style.fontFamily="Arial";
+//     }
